e9263bc11557370b7eb8b6bf73abc7b68b0a0179
Alan <582911658@qq.com>
2020-12-03 16:45:39 +0800

    更新

---
+++ 第五章/toppings.py
@@ -0,0 +1,8 @@
+requested_topping = ['mushroom','extra_cheese']
+if 'mushroom' in requested_topping :
+    print('Adding mushrooms!')
+if 'pepperoni' in requested_topping :
+    print('Adding pepperoni')
+if 'extra_cheese' in requested_topping :
+    print('Adding extra_cheese')
+print('\nFinish my pizza!')
