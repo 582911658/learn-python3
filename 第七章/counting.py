cf6a69aed6b7d887dd0e9c23e27ae7a931e3e709
alan <582911658@qq.com>
2020-12-09 16:15:41 +0800

    更新

--- 第七章/counting.py
+++ 第七章/counting.py
@@ -1,5 +1,14 @@
-current_number = 1
-while current_number <= 5:
-    print(current_number)
-    current_number +=1
-    
+print('10以内被2整除的数')
+current_number1 = 0
+while current_number1 < 10:
+    if current_number1 % 2 ==0:
+        print(current_number1)
+    current_number1 +=1
+    continue
+print('10以内不能被2整除的数')
+current_number2 = 0
+while current_number2 < 10:
+    current_number2 += 1
+    if current_number2 % 2 == 0:
+        continue
+    print(current_number2)
--- 第七章/parrot.py
+++ 第七章/parrot.py
@@ -1,6 +1,9 @@
 prompt = '\nTell me someth,and I will repeatg it back to you:'
-prompt += '\nEntel \'quit\' to end the program.'
-message =''
-while message != 'quit':
+prompt += '\nEntel \'quit\' to end the program.\n'
+active = True
+while active:
     message = input(prompt)
-    print(message)
+    if message == 'quit':
+        active = False
+    else :
+        print(message)
