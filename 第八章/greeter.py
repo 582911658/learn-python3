6e5d665045ba5b18bb93f85dd9d0dc0f57955d7a
alan <582911658@qq.com>
2020-12-10 08:30:04 +0800

    更新

--- 第八章/greeter.py
+++ 第八章/greeter.py
@@ -1,6 +1,4 @@
-from typing import DefaultDict
-
-def greet_user():
-    """ 简单的显示问候语"""
-    print('Hello!')
-greet_user()
+def greet_user(username):
+   ''' 简单的显示问候语'''
+    print('Hello! '+ username.title()+'!')
+greet_user('jessica')
