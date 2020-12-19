3b3b7d7ba2a2892b1b57b2e2a501fc58c7edcda5
alan <582911658@qq.com>
2020-12-19 22:11:21 +0800

    提交

--- 第七章/tempCodeRunnerFile.py
+++ 第七章/tempCodeRunnerFile.py
@@ -1,4 +1,6 @@
-if number % 2 ==0 :
-    print('\nThe number'+ str(number) +'is even.')
-else :
-    print('\n The number'+ str(number)+'is odd.')
+print('请输入一个数字判断是否为偶数')
+number = input('>')
+if int(number) % 2 == 0:
+    print('\nThe number '+str(number)+' is even.')
+else:
+    print('\n The number '+str(number)+' is odd.')
