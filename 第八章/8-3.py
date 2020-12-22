5e2fb11bf131b8001ff76f7513f1c11948befcac
alan <582911658@qq.com>
2020-12-22 14:44:33 +0800

    更新

---
+++ 第八章/8-3.py
@@ -0,0 +1,9 @@
+def make_shirt(size):
+    """
+    打印衣服的尺码
+    """
+    print('I love Python'+size.title()+' large.')
+    print('I love Python'+size.title()+' large.')
+
+
+make_shirt(str(62))
--- 第八章/pets.py
+++ 第八章/pets.py
@@ -1,7 +1,7 @@
 def describe_pet(animal_type, pet_name):
     """显示宠物信息"""
-    print('\nI have a ' + animal_type + '.')
-    print('\nIt\'s name is '+pet_name)
+    print('\nI have a ' + animal_type.title() + '.')
+    print('\nIt\'s name is '+pet_name.title()+'.')
 
 
-describe_pet('dog', 'Tom')
+describe_pet(animal_type='dog', pet_name='Tom')
