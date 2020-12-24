8e0182af26f75d87eda7a5969d87687c4f3fec07
alan <582911658@qq.com>
2020-12-24 15:31:20 +0800

    更新

--- 第八章/greeter.py
+++ 第八章/greeter.py
@@ -9,7 +9,14 @@ def get_formatted_name(first_name, last_name):
 # 这是一个无限循环
 while True:
     print('\nPlease tell me your name:')
+    print("\n enter 'q' at any time to quit")
+
     f_name = input('First name>')
+    if f_name == 'q':
+        break
     l_name = input('Last_name>')
+    if l_name == 'q':
+        break
+
     formatted_name = get_formatted_name(f_name, l_name)
     print('\nHello,'+formatted_name + '!')
